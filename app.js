0c0cb14c455b04d3b7a8c1e4d16309f544a11c0a
Hussein <harahaman123@gmail.com>
2024-06-20 21:46:37 +0100

    File updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Free World!I am '));
+app.get('/', (req, res) => res.send('Hello Free World!I '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
